1475cdab834915ac029066a84b20b2662895d291
Juan Molina <jfmolina2099@gmail.com>
2018-12-02 18:48:21 -0500

    Canvas is working with line and color history for multiplayer

---
+++ front-end/js/drawing.js
@@ -0,0 +1,94 @@
+//document.addEventListener('DOMContentLoaded', function() {
+  //Mouse obect
+  var mouse = {
+    click: false,
+    move: false,
+    position: {
+      x: 0,
+      y: 0
+    },
+    positionPrev: false,
+    color: 'black'
+  };
+
+  var canvas = document.getElementById('ctx');
+  var context = canvas.getContext('2d');
+  var width = 500;
+  var height = 500;
+  var socket = io();
+
+  // set canvas to full browser width/height
+  canvas.width = width;
+  canvas.height = height;
+
+  //Mouse click
+  canvas.onmousedown = function(e) {
+    mouse.click = true;
+  };
+  //Mouse unclick
+  canvas.onmouseup = function(e) {
+    mouse.click = false;
+  };
+
+  //Mouse movement in x and y
+  canvas.onmousemove = function(e) {
+    //Normalize mouse position to range 0.0 - 1.0
+    mouse.position.x = e.clientX / width;
+    mouse.position.y = e.clientY / height;
+    mouse.move = true;
+  };
+
+  //Color
+  //var buttonColor = document.getElementsByClass('colorPalette').addEventListener(click)
+  function colorPicker(obj) {
+    switch (obj.id) {
+        case "green":
+            mouse.color = "green";
+            break;
+        case "blue":
+            mouse.color = "blue";
+            break;
+        case "red":
+            mouse.color = "red";
+            break;
+        case "yellow":
+            mouse.color = "yellow";
+            break;
+        case "orange":
+            mouse.color = "orange";
+            break;
+        case "black":
+            mouse.color = "black";
+            break;
+    }
+}
+
+  //Draw line received from the server
+  socket.on('draw_line', function(data) {
+    var line = data.line;
+    context.strokeStyle = data.color;
+    context.lineWidth = 5;
+    context.beginPath();
+    context.moveTo(line[0].x * width, line[0].y * height);
+    context.lineTo(line[1].x * width, line[1].y * height);
+    context.stroke();
+  });
+
+  //Sends info to server every 25ms
+  function mainLoop() {
+     if (mouse.click && mouse.move && mouse.positionPrev) {
+        socket.emit('draw_line', {
+          line: [ mouse.position, mouse.positionPrev],
+          color: mouse.color
+        });
+        mouse.move = false;
+     }
+     mouse.positionPrev = {
+       x: mouse.position.x,
+       y: mouse.position.y
+     };
+     setTimeout(mainLoop, 25);
+  }
+  mainLoop();
+
+//});
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var express = require('express')
-var app = express()
-var server = require('http').Server(app);
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/front-end/index.html');
@@ -8,4 +8,33 @@ app.get('/', function (req, res) {
 
 app.use('/front-end', express.static(__dirname + '/front-end'));
 
-app.listen(3000);
+var server = app.listen(2000);
+console.log("Server started");
+
+// Socket IO
+var line_history = [];
+var line_color = [];
+var socket_list = [];
+
+var io = require('socket.io').listen(server);
+io.on('connection', function(socket) {
+  // Send history to new clients
+  for (var i in line_history){
+    socket.emit('draw_line', {
+      line: line_history[i],
+      color: line_color[i]
+    });
+  }
+
+  socket.on('draw_line', function(data) {
+    //Add to line_history
+    line_history.push(data.line);
+    line_color.push(data.color);
+    //Send to al clients
+    io.emit('draw_line', {
+      line: data.line,
+      color: data.color
+    });
+  });
+
+});
