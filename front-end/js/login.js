ee2130517f743e87070d4636c3fa0bdc3542fb6d
Juan Molina <jfmolina2099@gmail.com>
2018-12-16 01:13:45 -0500

    Have to create callback in main.js for socket validation

--- front-end/js/login.js
+++ front-end/js/login.js
@@ -1,15 +1,20 @@
+var socket = io();
 var logInDiv = document.getElementById('logInDiv');
 var nickname = document.getElementById('nickname');
 var confirmNickname = document.getElementById('confirmNickname');
-var socket = io();
+var gameDiv = document.getElementById('gameDiv');
 
 confirmNickname.onclick = function() {
   socket.emit('signIn',{
     nickname: nickname.value
-  })
-};
+  });
+}
 
-socket.on('signInResponse', function(data) {
+socket.on('hola',function(data) {
+  console.log(data);
+});
+
+socket.on('signResponse', function(data) {
   if (data.success) {
     logInDiv.style.display = 'none';
     gameDiv.style.display = 'inline-block';
